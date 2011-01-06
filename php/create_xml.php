349b214c0f5f2d3022bcf8acae4bda014d4e2dff
Florian Achleitner <florian.achleitner@student.tugraz.at>
2011-01-06 12:29:01 +0100

    Add client side XSL Transform and nicer error handling.

--- php/create_xml.php
+++ php/create_xml.php
@@ -1,70 +1,85 @@
 <?php
 	require('./Properties.class.php');
+	include_once 'error.php';
 	
 	$pnr = $_GET['pnr'];
 	$prop = new Properties();
 	$db = new mysqli( $prop->db_server, $prop->db_user, $prop->db_password, $prop->db_name );
 	
-	
-	if (mysqli_connect_errno() == 0)
+	if (mysqli_connect_errno())
 	{
-		$query = "SELECT p.name, o.o_name, a.date FROM Persons p
-				INNER JOIN Accountings a
-				ON p.p_nr = a.Persons_p_nr
-				INNER JOIN Offers o
-				ON a.Offers_o_id = o.o_id
-				WHERE p.p_nr = " . $pnr . ";";
-		
-		$result = $db->query( $query );
-		
-		if ($result->num_rows == 0)
-			echo 'Keine Einträge für den User';
-		else
-		{
-			$xmlcontent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-			$xmlcontent .= "<!-- all offers for specified person-->\n";
-			
-			$row = $result->fetch_object();
-			
-			$xmlcontent .= "<person_accountings>\n";
-			$xmlcontent .= "<person>\n<name>" . $row->name . "</name>\n</person>\n";
-			
-			$xmlcontent .= "<accountings>\n";
-			do {
-				$xmlcontent .= "<accounting>\n";
-		    	$xmlcontent .= "<offer>" . $row->o_name . "</offer>\n";
-		    	$xmlcontent .= "<date>" . $row->date . "</date>\n";
-		    	$xmlcontent .= "</accounting>\n";
-			} while($row = $result->fetch_object());
-			$xmlcontent .= "</accountings>\n";
-			
-			$xmlcontent .= "</person_accountings>\n";
-			
-			$xslpath = "../xsl/transform.xsl";
-			$xsltproc = new XsltProcessor();
+		errorhtml('Die Datenbank konnte nicht erreicht werden. Folgender Fehler trat auf: <strong>' .mysqli_connect_errno(). ' : ' .mysqli_connect_error(). '</strong>');
+		die;
+	}
 
-			// DOM Dokument initiieren und xsl-datei laden
-			$xsl = new DomDocument;
-			$xsl->load($xslpath);
-			$xsltproc->importStylesheet($xsl);
-			
-			// DOM initiieren und xml-datei laden
-			$xml_doc = new DomDocument;
-			$xml_doc->loadXML($xmlcontent);
-			
-			// Transformation
-			if ($html = $xsltproc->transformToXML($xml_doc))
-			   echo $html;
-			else 
-				echo "XSL Transformation failed.";
-			
-		}
+	$query = "SELECT p.name, o.o_name, a.date FROM Persons p
+			INNER JOIN Accountings a
+			ON p.p_nr = a.Persons_p_nr
+			INNER JOIN Offers o
+			ON a.Offers_o_id = o.o_id
+			WHERE p.p_nr = " . $pnr . ";";
+	
+	$result = $db->query( $query );
+	
+	if ($result->num_rows == 0)
+	{
+		errorhtml( 'Keine Einträge für den User');
+		die;
+	}
+	$xslpath = "../xsl/transform.xsl";
+
+	$xmlcontent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+	if($_GET['serverclient'] == 'client')
+		$xmlcontent .= "<?xml-stylesheet type=\"text/xsl\" href=\"$xslpath\"?>";
+
+	$xmlcontent .= "<!-- all offers for specified person-->\n";
+	
+	$row = $result->fetch_object();
+	
+	$xmlcontent .= "<person_accountings>\n";
+	$xmlcontent .= "<person>\n<name>" . $row->name . "</name>\n</person>\n";
+	
+	$xmlcontent .= "<accountings>\n";
+	do {
+		$xmlcontent .= "<accounting>\n";
+    	$xmlcontent .= "<offer>" . $row->o_name . "</offer>\n";
+    	$xmlcontent .= "<date>" . $row->date . "</date>\n";
+    	$xmlcontent .= "</accounting>\n";
+	} while($row = $result->fetch_object());
+	$xmlcontent .= "</accountings>\n";
+	
+	$xmlcontent .= "</person_accountings>\n";
+	
+	if($_GET['serverclient'] == 'client')
+	{
+		header('Content-Type: text/xml');
+		echo $xmlcontent;
 	}
 	else
 	{
-	    // Es konnte keine Datenbankverbindung aufgebaut werden
-	    echo 'Die Datenbank konnte nicht erreicht werden. Folgender Fehler trat auf: <strong>' .mysqli_connect_errno(). ' : ' .mysqli_connect_error(). '</strong>';
-	}
+		$xsltproc = new XsltProcessor();
+
+		// DOM Dokument initiieren und xsl-datei laden
+		$xsl = new DomDocument;
+		$xsl->load($xslpath);
+		$xsltproc->importStylesheet($xsl);
 	
+		// DOM initiieren und xml-datei laden
+		$xml_doc = new DomDocument;
+		$xml_doc->loadXML($xmlcontent);
+	
+		// Transformation
+		if ($html = $xsltproc->transformToXML($xml_doc)) {
+			echo $html;
+		}
+		else 
+		{
+			errorhtml( "XSL Transformation failed.");
+			die;
+		}
+	}
+	//echo "<!-- " . htmlspecialchars($xmlcontent) . ">";
+
 	$db->close();
 ?>
