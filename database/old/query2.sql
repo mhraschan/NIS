f4060cbecaca7cc20f8c6725579d9d287eb46f9c
Florian Achleitner <florian.achleitner@student.tugraz.at>
2011-01-12 20:08:13 +0100

    README and cleanup

--- database/old/query2.sql
+++
@@ -1,6 +0,0 @@
-SELECT p.name FROM Persons p
-	WHERE p.p_nr IN 
-		(SELECT a.Persons_p_nr FROM Accountings a 
-			INNER JOIN Offers o ON a.Offers_o_id = o.o_id
-			WHERE a.date = '2009-11-24' 
-			AND o.o_name ='Surfing');
